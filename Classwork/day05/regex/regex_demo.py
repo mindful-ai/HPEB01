1c29f8418cd9608f4fdd6a430794e83c901f1af6
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2019-12-17 11:19:55 +0530

    Add files via upload

---
+++ Classwork/day05/regex/regex_demo.py
@@ -0,0 +1,14 @@
+import re
+
+expr = '37.0 degree centigrade is equal to +98.6 farhenheit'
+
+pattern = r'-|\+?\d+\.?\d*|\.?\d+'
+
+print (expr)
+print ('Trying to find a floating point numbers in the statement...')
+
+match = re.findall(pattern, expr)
+if match:
+    print ('Following numbers were found', match)
+else:
+    print ('Seems like there is no floating point number')
